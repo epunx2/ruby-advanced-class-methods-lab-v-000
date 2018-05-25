dd9d4f66b3d8889ab36cd9602c22bb147fcf4770
Eric Chrobak <eric@ericchrobak.com>
2018-05-25 00:15:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.detect {|person| person.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    song = Song.find_by_name(name)
+    if song == nil
+      song = Song.create_by_name(name)
+    end
+    song
+  end
+
+  def self.alphabetical
+    new_array = []
+    sorted_songs = []
+    Song.all.each do |person|
+      new_array << person.name
+    end
+    new_array = new_array.sort
+    new_array.each do |song|
+      sorted_songs << @@all.detect {|person| person.name == song}
+    end
+    sorted_songs
+  end
+
+  def self.new_from_filename(name)
+    name = name.chomp(\.\w+$)
+    artist = name[/^\w+\b/]
+    song = name.chomp(/^\w+\b/)
+
+    new_song = self.new_by_name(name)
+    @artist_name = artist
+    song
+  end
+
 end
